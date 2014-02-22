2fd962ff460effd6e003706c1228860f99ae1d2a
Eric Brown <eric.d.brown23@gmail.com>
2014-02-21 22:13:09 -0500

    Fully setup index.html to serve from django

--- interface/views.py
+++ interface/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.template import Context, RequestContext
 
 # Create your views here.
+def index(request):
+    c = RequestContext(request, {})
+    return render(request, 'interface/index.html', c)
