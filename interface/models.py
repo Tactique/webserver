940fa9aa2d138ca8c4f19af384310138d8e24bf4
Eric Brown <eric.d.brown23@gmail.com>
2014-02-23 22:37:16 -0500

    Provide initial, pretty useless, models

--- interface/models.py
+++ interface/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class HasSprite(models.Model):
+    spriteName = models.CharField(max_length=50)
+
+    class Meta:
+        abstract = True
+
+class Cell(HasSprite):
+    cType = models.PositiveIntegerField()
+
+class Unit(HasSprite):
+    distance = models.PositiveIntegerField()
+    movementType = models.CharField(max_length=50)
