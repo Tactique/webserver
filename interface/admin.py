86eb9327809bb4980ffbbe6169650eb77a21faad
Eric Brown <eric.d.brown23@gmail.com>
2014-03-02 21:15:23 -0500

    Add new models to admin interface

--- interface/admin.py
+++ interface/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from interface.models import GameUser, Cell, Unit, World
 
 # Register your models here.
+admin.site.register(GameUser)
+admin.site.register(Cell)
+admin.site.register(Unit)
+admin.site.register(World)
