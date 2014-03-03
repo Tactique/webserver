36fb824b3b3a979eb27236e8181c7f4f294c212c
Eric Brown <eric.d.brown23@gmail.com>
2014-03-02 19:52:45 -0500

    Simplify urls by not using base url

--- interface/urls.py
+++ interface/urls.py
@@ -5,5 +5,5 @@ from interface import views
 urlpatterns = patterns('',
     url(r'^tests/$', views.tests, name='tests'),
     url(r'^editor/$', views.editor, name='editor'),
-    url(r'$', views.index, name='index'),
+    url(r'play/$', views.index, name='index'),
 )
