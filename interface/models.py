cf8e8d0a83472f0ab51c6a6af9c894429ce62aea
Eric Brown <eric.d.brown23@gmail.com>
2014-03-02 19:53:07 -0500

    Simple models

--- interface/models.py
+++ interface/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class GameUser(models.Model):
+    user = models.OneToOneField(User)
+    wins = models.PositiveIntegerField(default=0)
+    loss = models.PositiveIntegerField(default=0)
 
 # Create your models here.
 class HasSprite(models.Model):
@@ -8,8 +14,12 @@ class HasSprite(models.Model):
         abstract = True
 
 class Cell(HasSprite):
-    cType = models.PositiveIntegerField()
+    cType = models.PositiveIntegerField(default=0)
 
 class Unit(HasSprite):
-    distance = models.PositiveIntegerField()
+    distance = models.PositiveIntegerField(default=1)
     movementType = models.CharField(max_length=50)
+
+# I'm so lazy...
+class World(models.Model):
+    lump = models.TextField()
