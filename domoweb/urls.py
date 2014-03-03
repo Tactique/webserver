c6137b538ba419f2bfdd521c672c288488a1c2d7
Eric Brown <eric.d.brown23@gmail.com>
2014-03-02 19:51:39 -0500

    Remove some django templated code

--- domoweb/urls.py
+++ domoweb/urls.py
@@ -5,10 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^', include('interface.urls')),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
 
