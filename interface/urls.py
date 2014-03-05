5eeba463d6fd5e33a6db7eea987e180b2c5d2509
Eric Brown <eric.d.brown23@gmail.com>
2014-03-04 21:47:35 -0500

    Minor, potentially useless, change?

--- interface/urls.py
+++ interface/urls.py
@@ -5,7 +5,7 @@ from interface import views, ajax_views
 urlpatterns = patterns('',
     url(r'^tests/$', views.tests, name='tests'),
     url(r'^editor/$', views.editor, name='editor'),
-    url(r'play/$', views.index, name='index'),
+    url(r'^play/$', views.index, name='index'),
     # Ajax network calls
     url(r'^info/cell/$', ajax_views.get_cells),
 )
