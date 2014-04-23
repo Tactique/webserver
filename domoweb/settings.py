30491c9986c20788744ac9702fbbaff771a9472d
Eric Brown <eric.d.brown23@gmail.com>
2014-04-22 21:51:37 -0400

    Update static paths to account for jswars changes

--- domoweb/settings.py
+++ domoweb/settings.py
@@ -81,9 +81,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/jswars/assets/'
+STATIC_URL = '/jswars/'
 
-JSWARS_DIR = os.path.join(BASE_DIR, 'jswars/assets')
+JSWARS_DIR = os.path.join(BASE_DIR, 'jswars')
 
 STATICFILES_DIRS = (
     JSWARS_DIR,
