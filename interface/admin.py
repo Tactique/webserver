45aa63132be8097892af8090480f5c39d2f3ba7b
Eric Brown <eric.d.brown23@gmail.com>
2014-04-27 21:07:09 -0400

    Update admin page with new models

--- interface/admin.py
+++ interface/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from interface.models import GameUser, Cell, Unit, World
+from interface.models import GameUser, Cell, Unit, World, LoginData, ResponseTemplate
 
 # Register your models here.
 admin.site.register(GameUser)
 admin.site.register(Cell)
 admin.site.register(Unit)
 admin.site.register(World)
+admin.site.register(LoginData)
+admin.site.register(ResponseTemplate)
