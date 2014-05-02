93cb38cbee45b4dc954057e53d1acdb3517fb330
Quinten Palmer <quintenpalmer@gmail.com>
2014-05-01 23:53:27 -0400

    New Models and SeedDB for all pieces of unit

--- interface/management/commands/SeedDB.py
+++ interface/management/commands/SeedDB.py
@@ -1,5 +1,14 @@
 from django.core.management.base import BaseCommand
-from interface.models import Cell
+from interface.models import (
+    Cell,
+    WeaponType,
+    Weapon,
+    ArmorType,
+    Armor,
+    SpeedMap,
+    Movement,
+    Unit,
+)
 
 import os
 import csv
@@ -9,19 +18,138 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            TERRAIN_PATH = '%s/common/resources/terrain.csv' % (os.environ["DOMOROOT"],)
-            self.clearCells()
-            # Should be the path to the common types list
-            with open(TERRAIN_PATH, 'r') as tFile:
-                terrain_reader = csv.reader(tFile)
-                next(terrain_reader, None)
-                for cType, spriteName in terrain_reader:
-                    print("Creating Cell(%s, %s)" % (cType, spriteName))
-                    Cell(spriteName=spriteName, cType=cType).save()
+            resources_path = '%s/common/resources/' % (os.environ['DOMOROOT'],)
         except KeyError:
             print("Please define the $DOMOROOT environment variable to your domoco dir")
+            return
+        self.clearCells()
+        cell_types = self.seed_cells(resources_path)
+        weapon_types = self.seed_weapon_types(resources_path)
+        weapons = self.seed_weapons(resources_path, weapon_types)
+        armor_types = self.seed_armor_types(resources_path)
+        armors = self.seed_armors(resources_path, armor_types)
+        speed_maps = self.seed_speed_maps(resources_path)
+        movements = self.seed_movements(resources_path, speed_maps)
+        self.seed_units(resources_path, weapons, armors, movements)
 
     def clearCells(self):
         print("Clearing the Cells")
         for cell in Cell.objects.all():
             cell.delete()
+
+    def seed_cells(self, resources_path):
+        with open(resources_path + 'terrain.csv', 'r') as file_:
+            reader = csv.reader(file_)
+            next(reader, None)
+            for cellType, spriteName in reader:
+                print("Creating Cell(%s)" % (
+                    ', '.join((cellType, spriteName))))
+                Cell(spriteName=spriteName, cellType=cellType).save()
+
+    def seed_weapon_types(self, resources_path):
+        dbWeaponTypes = {}
+        with open(resources_path + 'weaponTypes.csv', 'r') as file_:
+            reader = csv.reader(file_)
+            next(reader, None)
+            for weaponType, name in reader:
+                print("Creating WeaponType(%s)" % (
+                    ', '.join((weaponType, name))))
+                dbWeaponType = WeaponType(name=name, weaponType=weaponType)
+                dbWeaponType.save()
+                dbWeaponTypes[name] = dbWeaponType
+        return dbWeaponTypes
+
+    def seed_weapons(self, resources_path, weapon_types):
+        dbWeapons = {}
+        with open(resources_path + 'weapons.csv', 'r') as file_:
+            reader = csv.reader(file_)
+            next(reader, None)
+            for name, weaponType, power, minRange, maxRange in reader:
+                print("Creating WeaponType(%s)" % (
+                    ', '.join((name, weaponType, power, minRange, maxRange))))
+                weapon = Weapon(
+                    name=name,
+                    weaponType=weapon_types[weaponType],
+                    power=power,
+                    minRange=minRange,
+                    maxRange=maxRange)
+                weapon.save()
+                dbWeapons[name] = weapon
+        return dbWeapons
+
+    def seed_speed_maps(self, resources_path):
+        dbSpeedMaps = {}
+        with open(resources_path + 'speedMaps.csv', 'r') as file_:
+            reader = csv.reader(file_, quotechar='\'')
+            next(reader, None)
+            for name, jsonSpeeds in reader:
+                print("Creating SpeedMap(%s)" % (
+                    ', '.join((name, jsonSpeeds))))
+                dbSpeedMap = SpeedMap(name=name, speeds=jsonSpeeds)
+                dbSpeedMap.save()
+                dbSpeedMaps[name] = dbSpeedMap
+        return dbSpeedMaps
+
+    def seed_movements(self, resources_path, speed_maps):
+        dbMovements = {}
+        with open(resources_path + 'movements.csv', 'r') as file_:
+            reader = csv.reader(file_)
+            next(reader, None)
+            for name, distance, speedMap in reader:
+                print("Creating Movement(%s)" % (
+                    ', '.join((name, distance, speedMap))))
+                movement = Movement(
+                    name=name,
+                    distance=distance,
+                    speedMap=speed_maps[speedMap])
+                movement.save()
+                dbMovements[name] = movement
+        return dbMovements
+
+    def seed_armor_types(self, resources_path):
+        dbArmorTypes = {}
+        with open(resources_path + 'armorTypes.csv', 'r') as file_:
+            reader = csv.reader(file_)
+            next(reader, None)
+            for armorType, name in reader:
+                print("Creating ArmorType(%s)" % (
+                    ', '.join((armorType, name))))
+                dbArmorType = ArmorType(name=name, armorType=armorType)
+                dbArmorType.save()
+                dbArmorTypes[name] = dbArmorType
+        return dbArmorTypes
+
+    def seed_armors(self, resources_path, armor_types):
+        dbArmors = {}
+        with open(resources_path + 'armors.csv', 'r') as file_:
+            reader = csv.reader(file_)
+            next(reader, None)
+            for name, strength, armorType in reader:
+                print("Creating Armor(%s)" % (
+                    ', '.join((name, armorType, strength))))
+                armor = Armor(
+                    name=name,
+                    armorType=armor_types[armorType],
+                    strength=strength)
+                armor.save()
+                dbArmors[name] = armor
+        return dbArmors
+
+    def seed_units(self, resources_path, weapons, armors, movements):
+        with open(resources_path + 'units.csv', 'r') as file_:
+            reader = csv.reader(file_)
+            next(reader, None)
+            for name, health, attack_one, attack_two, armor, movement in reader:
+                print("Creating Unit(%s)" % (
+                    ', '.join((name, health, attack_one, attack_two, armor, movement))))
+                if attack_two != 'null':
+                    second_weapon = weapons[attack_two]
+                else:
+                    second_weapon = None
+                Unit(
+                    name=name,
+                    health=health,
+                    attack_one=weapons[attack_one],
+                    attack_two=second_weapon,
+                    armor=armors[armor],
+                    movement=movements[movement]).save()
--- interface/models.py
+++ interface/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+MAX_CHAR_LENGTH = 128
+MAX_JSON_LENGTH = 526
+
 class GameUser(models.Model):
     user = models.OneToOneField(User)
     wins = models.PositiveIntegerField(default=0)
@@ -9,26 +12,60 @@ class GameUser(models.Model):
 class LoginData(models.Model):
     userid = models.PositiveIntegerField(default=0)
     # This max_length could be set to the precise length. Or a new field could be made
-    token = models.CharField(max_length=50)
+    token = models.CharField(max_length=MAX_CHAR_LENGTH)
 
 # Create your models here.
 class HasSprite(models.Model):
-    spriteName = models.CharField(max_length=50)
+    spriteName = models.CharField(max_length=MAX_CHAR_LENGTH)
 
     class Meta:
         abstract = True
 
 class Cell(HasSprite):
-    cType = models.PositiveIntegerField(default=0)
+    cellType = models.PositiveIntegerField()
+
+class WeaponType(models.Model):
+    name = models.CharField(max_length=MAX_CHAR_LENGTH)
+    weaponType = models.PositiveIntegerField()
+
+class Weapon(HasSprite):
+    weaponType = models.ForeignKey('WeaponType')
+    name = models.CharField(max_length=MAX_CHAR_LENGTH)
+    power = models.PositiveIntegerField()
+    minRange = models.PositiveIntegerField()
+    maxRange = models.PositiveIntegerField()
+
+class ArmorType(models.Model):
+    armorType = models.PositiveIntegerField()
+    name = models.CharField(max_length=MAX_CHAR_LENGTH)
+
+class Armor(HasSprite):
+    name = models.CharField(max_length=MAX_CHAR_LENGTH)
+    strength = models.PositiveIntegerField()
+    armorType = models.ForeignKey('ArmorType')
+
+class SpeedMap(models.Model):
+    name = models.CharField(max_length=MAX_CHAR_LENGTH)
+    speeds = models.CharField(max_length=MAX_JSON_LENGTH)
+
+class Movement(HasSprite):
+    distance = models.PositiveIntegerField(default=1)
+    name = models.CharField(max_length=MAX_CHAR_LENGTH)
+    speedMap = models.ForeignKey('SpeedMap')
 
 class Unit(HasSprite):
-    distance = models.PositiveIntegerField(default=1)
-    movementType = models.CharField(max_length=50)
+    name = models.CharField(max_length=MAX_CHAR_LENGTH)
+    health = models.PositiveIntegerField()
+    movement = models.ForeignKey('Movement')
+    attack_one = models.ForeignKey('Weapon')
+    attack_two = models.ForeignKey(
+        'Weapon', blank=True, null=True, related_name='attack_two_weapon')
+    armor = models.ForeignKey('Armor')
 
 # I'm so lazy...
 class World(models.Model):
     lump = models.TextField()
 
 class ResponseTemplate(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=MAX_CHAR_LENGTH)
     JSON = models.TextField()
