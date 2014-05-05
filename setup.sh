21976b139e50813c304a5f65ec970428877016de
Quinten Palmer <quintenpalmer@gmail.com>
2014-05-05 18:38:19 -0400

    Fuller setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 
-python manage.py syncdb
+function usage() {
+	echo "either supply no arguments or \"deps\""
+}
+
+function syncdb() {
+	python manage.py syncdb --noinput
+}
+
+function install_deps() {
+	sudo pip install -r requirements.txt
+}
+
+function main() {
+	if [ $# -lt 1 ]; then
+		syncdb
+	else
+		if [ $1 == 'deps' ]; then
+			install_deps
+		else
+			usage
+		fi
+	fi
+}
+
+main $@
